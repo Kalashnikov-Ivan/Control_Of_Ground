8b72ab64a22b7c92a1208a459b980cfdadeaf238
Kalashnikov Ivan <steindvart.st@gmail.com>
2020-05-15 16:16:19 +0300

    Develop of Unit. Start testing SFML

--- src/source/Unit.hpp
+++ src/source/Unit.hpp
@@ -1,16 +1,35 @@
 namespace cog
 {
-	class Unit
-	{
-		public:
 
-			Unit();
-			~Unit();
+class Unit : public sf::Sprite
+{
+public:
 
+	//Constructors
+	Unit();
+	Unit(std::string name, double m_hp, double m_power);
+	~Unit();
 
+	//Name
+	std::string get_name() const;
+	void set_name(const std::string& name);
 
-		private:
+	//HP
+	double get_hp() const;
+	void set_hp(const double& hp);
 
-	};
+	void get_damage(double damage);
 
-}
+	//Power of damage
+	double get_power() const;
+	void set_power(const double& m_power);
+
+private:
+	static size_t id;
+
+	std::string m_name;
+	double m_hp;
+	double m_power;
+};
+
+} // namespace cog
--- src/source/main.cpp
+++ src/source/main.cpp
@@ -2,8 +2,9 @@
 
 int main(int argc, char* argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-    sf::CircleShape shape(200.f);
+    sf::RenderWindow window{ sf::VideoMode(400, 400), "SFML works!" };
+
+    sf::CircleShape shape{ 200.f };
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
@@ -15,8 +16,13 @@ int main(int argc, char* argv[])
                 window.close();
         }
 
+        //Clearing of frame
         window.clear();
+
+        //Drawling of frame
         window.draw(shape);
+
+        //Display in window all of has been draw
         window.display();
     }
 
